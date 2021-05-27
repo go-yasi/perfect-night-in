65f104e474850005c968b9e36dfc95e624e799a2
sclo00ps <louginescott@gmail.com>
2021-05-27 17:32:36 -0400

    activities api request/response

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-$('select.dropdown')
-  .dropdown()
-;
+// $('select.dropdown')
+// .dropdown()
+// ;
+
+var boredURL = "https://www.boredapi.com/api/activity"
+var typeparam = 'type';
+var participantparm = 'participants';
+var priceparam = 'price';
+
+function getActivities() {
+fetch(boredURL, {
+})
+
+  .then(function(response){
+    return response.json();
+    })
+
+    .then(function (data){
+      console.log(data);
+    });
+ console.log("test runs"); 
+}
+
+for (let i = 0; i < 5; i++) {
+  getActivities();
+  
+}
+
+
+    
+    
